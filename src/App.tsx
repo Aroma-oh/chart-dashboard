06bcb5246c16d2231e53eeb8835015b42272197b
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-12 10:54:20 +0900

    feat: App 컴포넌트 리코일, theme, 레이아웃 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { RecoilRoot } from 'recoil';
+
+import theme from 'styles/theme';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from 'styles/globalStyles';
+import Dashboard from 'components/Dashboard';
+
 function App() {
-  return <>hi</>;
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Dashboard />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
 }
 
 export default App;
