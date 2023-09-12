75e7e9e559ea287cef0217c381268e582965daa7
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-12 10:55:09 +0900

    feat: bar, area 복합 그래프 디스플레이 구현

---
+++ src/components/ChartDisplay.tsx
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+import mockData from 'mock_data.json';
+import { groupDataByDate } from 'utils/groupDataByDate';
+import {
+  ComposedChart,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Cell,
+} from 'recharts';
+import theme from 'styles/theme';
+import CustomTooltip from './CustomTooltip';
+import FilterComponent from './Filter';
+import { useFilter } from 'hooks/useFilter';
+import { useEffect } from 'react';
+
+const ChartDisplay = () => {
+  const data = groupDataByDate(mockData.response);
+  const { selectedDate, selectedId } = useFilter(data);
+
+  console.info(data);
+
+  const formatNumber = (item: number) => item.toLocaleString();
+  useEffect(() => {
+    console.info(selectedDate);
+  }, [selectedDate]);
+
+  return (
+    <Box>
+      <FilterComponent data={data} />
+      <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data[selectedDate]}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="time" scale="band" className="XAxis" />
+          <Tooltip content={<CustomTooltip />} formatter={formatNumber} />
+          <YAxis yAxisId="left" tickFormatter={formatNumber} />
+          <YAxis yAxisId="right" orientation="right" />
+          <Legend
+            align="center"
+            payload={[
+              { value: 'value_area', type: 'square', color: theme.colors.areaStroke },
+              { value: 'value_bar', type: 'square', color: theme.colors.barStroke },
+            ]}
+          />
+          <Bar dataKey="value_bar" yAxisId="left" barSize={20} stroke={theme.colors.barStroke}>
+            {data[selectedDate]?.map((item, idx) => (
+              <Cell
+                key={idx}
+                fill={item.id === selectedId ? theme.colors.barFillHighlight : theme.colors.barFill}
+              />
+            ))}
+          </Bar>
+          <Area
+            type="monotone"
+            dataKey="value_area"
+            yAxisId="right"
+            fill={theme.colors.areaFill}
+            stroke={theme.colors.areaStroke}
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </Box>
+  );
+};
+
+export default ChartDisplay;
+
+const Box = styled.section`
+  background-color: ${({ theme }) => theme.colors.bgBoard};
+  font-size: 10px;
+
+  height: 650px;
+  max-width: 95vw;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
